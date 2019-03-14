32711a7129e46be5797d301a0f91d749fb42001f
khaundaditya <aditya.khaund51@gmail.com>
2019-03-14 09:29:21 +0530

    Simple accounting application for demo

---
+++ __init__.py
@@ -0,0 +1,11 @@
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Initialize the application.
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+
+# Import the views file for routing.
+import views
